59970e217d9eadb1d2fbc0039d848f6a1c29fcc8
chunhao.wch <chunhao.wch@alibaba-inc.com>
2021-07-30 09:51:11 +0800

    提交

---
+++ java-base/src/main/java/nomouse/base/LockTest.java
@@ -0,0 +1,15 @@
+package nomouse.base;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * @author nomouse
+ * @date 2021/7/29
+ */
+public class LockTest {
+
+    public void lockTest(){
+        Lock lock = new ReentrantLock();
+    }
+}
---
+++ java-base/src/main/java/nomouse/base/ThreadPoolTest.java
@@ -0,0 +1,8 @@
+package nomouse.base;
+
+/**
+ * @author nomouse
+ * @date 2021/7/29
+ */
+public class ThreadPoolTest {
+}
